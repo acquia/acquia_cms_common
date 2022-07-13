c169815bfcaea4d77f97309f848b80bbe21c2eca
Chandan Singh <chandan.singh@acquia.com>
2022-07-13 14:31:14 +0530

    ACMS-1275: Implement CI/CD for common module.

--- tests/src/Functional/NodeBreadcrumbTest.php
+++ tests/src/Functional/NodeBreadcrumbTest.php
@@ -109,7 +109,7 @@ class NodeBreadcrumbTest extends BrowserTestBase {
   }
 
   /**
-   * Returns the breadcrumb contents of the current page in the internal browser.
+   * Returns the breadcrumb contents of current page in the internal browser.
    */
   protected function getBreadcrumbParts() {
     $parts = [];
@@ -125,4 +125,5 @@ class NodeBreadcrumbTest extends BrowserTestBase {
     }
     return $parts;
   }
+
 }
